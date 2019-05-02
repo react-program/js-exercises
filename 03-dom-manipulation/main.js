8234c186d4ad744e5da9797c3602e874fcfbffa8
Govind <govindaraj.kannan@krds.fr>
2019-05-02 15:29:22 +0530

    03-DOM-Manipulation MVC pattern solution v1

--- 03-dom-manipulation/main.js
+++ 03-dom-manipulation/main.js
@@ -1,52 +1,60 @@
 
 //DOM Manipulation
-let app_li = document.querySelectorAll(".classifiedColumn > li:first-child"); //Get the parent columns to append the child nodes
-const x = document.getElementById("pocket"); //Declare variable for eventListener
-//Call EventListener Function
-x.addEventListener("click", elementClick); 
+var M = {}, V = {}, C = {};
+let matchingIndex,text = '';
 
-//Trigger Events on clicking the element
-function elementClick(e) { 
-    let matchingIndex = matchUl(e.target.innerText[0]); //Get the first character of the innertext
-    let text = e.target;
-    //If matchingIndex is number then call Append List function
-    if(!isNaN(matchingIndex)){
-        appendLi(matchingIndex,text); 
+M = {
+    data: {
+        appLi : document.querySelectorAll(".classifiedColumn > li"),
+        pocketLi : document.querySelectorAll("#pocket ul > li").length
+    }, 
+    setData : function(d){
+        this.data.appLi = d.appLi;
+        this.data.pocketLi = d.pocketLi;
+    },
+    getData : function(){        
+        return data;
     }
 }
 
-//Check the matching parent of selected itemt
-function matchUl(ulText){
-    for(let i=0;i<app_li.length;i++){
-        let liChar = app_li[i].innerText.slice(-1);
-        if(liChar == ulText){ 
-            return i;
+V = {
+    update: function(matchingIndex,text){        
+        //If matchingIndex is number then call sortList function
+        if(!isNaN(matchingIndex)){        
+            let ul = document.querySelectorAll("ul")[matchingIndex];
+            ul.appendChild(text);
+            //Sort Function
+            Array.from(ul.getElementsByTagName("LI"))
+            .sort(function(a, b){
+            a = a.textContent[1];
+            b = b.textContent[1]; 
+            if(!isNaN(a) && !isNaN(b)){
+                return a < b ? -1 : (a > b) ? 1 : 0;
+            }
+            }).forEach(li => {ul.appendChild(li)});
         }
     }
 }
 
-//Append child list to parent
-function appendLi(index,text){
-    let ul = document.querySelectorAll("ul")[index];
-    ul.appendChild(text);
-    sortList(ul);
-}
-  
-function sortList(ul) {  
-    //Sort Function
-    Array.from(ul.getElementsByTagName("LI"))
-      .sort(function(a, b){
-        a = a.textContent[1];
-        b = b.textContent[1]; 
-        if(!isNaN(a) && !isNaN(b)){
-            return a < b ? -1 : (a > b) ? 1 : 0;
+C = {
+    model: M,
+    view: V,
+    handler: function(e){
+       let app_li = M.data.appLi;
+        if(e.target.nodeName=="LI"){
+            let ulText = e.target.innerText[0]; //Get the first character of the innertext
+            text = e.target;
+            for(let i=0;i<app_li.length;i++){
+                let liChar = app_li[i].innerText.slice(-1);
+                if(liChar == ulText){ 
+                    matchingIndex = i;
+                }
+            }
         }
-      }).forEach(li => {ul.appendChild(li)});
+        this.view.update(matchingIndex,text);
+    }
+}
 
-      let pocket_li = document.querySelectorAll("#pocket ul > li").length; //Check the length of elements onclick
-      //Remove Div if all elements are sorted
-      if(pocket_li==0){
-          var element = document.getElementById("pocket");
-          element.parentNode.removeChild(element);
-      }
-  }  
+document.querySelector("#pocket").addEventListener("click", function(e){
+    C.handler.call(C,e);
+}); 
