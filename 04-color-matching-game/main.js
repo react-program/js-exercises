a0f7a4ab60f7c56daf09de1b4e18785adfa00ca7
Govind <govindaraj.kannan@krds.fr>
2019-05-15 16:13:03 +0530

    04 Color Matching Game MVC solution

--- 04-color-matching-game/main.js
+++ 04-color-matching-game/main.js
@@ -1,2 +1,21 @@
 //Color matching game
-console.log('init');
+const model = {
+    el : document.getElementById('board'),
+    colors : ['red', 'green', 'blue', 'orange', 'yellow'],
+    color : ''
+}
+const controller = {
+    elAction : (e) => {
+        color = model.colors[Math.floor(Math.random() * model.colors.length)];
+        view.renderView(e,color);
+    }
+}
+const view = {
+    renderView : (e,color) => {
+        //console.log(e.target)
+        e.target.style.backgroundColor = color;
+        //console.log(e.target+" -> "+color);
+    }
+}
+
+model.el.addEventListener('click', controller.elAction);
