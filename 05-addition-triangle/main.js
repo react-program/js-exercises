de3cb324811ab49c05c03e8e3459f319ea6fc34c
Govind <govindaraj.kannan@krds.fr>
2019-06-04 11:23:32 +0530

    05 Triangle Addition Solution 1

--- 05-addition-triangle/main.js
+++ 05-addition-triangle/main.js
@@ -1,43 +1,36 @@
 const model = {
     button : document.getElementById('generate'),
     inputValues : document.getElementById('inputValues'),
-    divTriangle : document.getElementById('triangle'),
+    divTriangle : document.getElementById('row')
 };
 const controller = {
     generateTriangle : () => {
         if(model.inputValues.value.length > 0){
-            let splitInputs = model.inputValues.value.split(" ");
+            let splitInputs = model.inputValues.value.trim().split(" ");
             controller.buildTriangle(splitInputs);
-            //console.log("Length ",model.inputValues.value.length);
         }
     },
     buildTriangle : (array) => {
         let new_array = [];
-        //console.log("leng", splitInputs.length);
-        if(array.length >= 2){
+        if(array.length >= 1){
             array.forEach(function (i,j) {
-                console.log(j);
                 if(parseInt(array.length) -1 != j){
                     let sum = parseInt(array[j]) + parseInt(array[j+1]);
-                    //console.log("Sum '",sum);
                     new_array.push(sum);
                 }
             });
-            console.log(new_array);
-            view.renderTriangle(new_array);
-            controller.buildTriangle(new_array);
+            setTimeout(function() {
+                view.renderTriangle(array);
+                controller.buildTriangle(new_array);
+            },1000);           
         }
     }
 };
 const view = {
     renderTriangle : (new_array) => {
-        var el = document.getElementById("triangle");
-        // var height = el.offsetHeight;
-        // var newHeight = height + 200;
-        // el.style.height = newHeight + 'px';
-        el.style.height = '200px';
-        el.style.textAlign = 'center';
-        model.divTriangle.prepend(new_array);
+        var el = document.createElement('p');
+        el.innerHTML = new_array.toString().replace( /,/g, "  " );
+        model.divTriangle.insertAdjacentHTML("afterend", "<p>"+el.innerHTML+"</p>");        
     }
 };
-model.button.addEventListener('click', controller.generateTriangle)
+model.button.addEventListener('click', controller.generateTriangle);
