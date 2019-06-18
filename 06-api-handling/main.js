e3ee29c81830d78e134588655647c5d8d3c5b7cb
Govind <govindaraj.kannan@krds.fr>
2019-06-18 17:20:26 +0530

    06 API Handling - Removed MVC

--- 06-api-handling/main.js
+++ 06-api-handling/main.js
@@ -1,42 +1,56 @@
-const model = {
-   contentDiv : document.getElementById("content"),
-   modalDiv : document.getElementById("modal-body")
-};
-const controller = {
-    init : _ => {
-        fetch('https://jsonplaceholder.typicode.com/posts').then(resp => resp.json()).then(resp => view.renderPosts(resp));
-    },
-    getAuthorDetails : (id) => {
-        fetch('https://jsonplaceholder.typicode.com/users/'+id).then(resp => resp.json()).then(resp => view.renderPopup(resp));
-    }
-};
-const view = {
-    renderPosts : (resp) => {
-        let html = '';
-        resp.forEach(element => {
-            html += '<div class="col-md-4">'+
-                            '<div class="card border-success mb-4">'+
-                                '<div class="card-header">'+element['title']+'</div>'+
-                                '<div class="card-body text-secondary">'+
-                                    '<p class="card-text">'+element['body']+'</p>'+
-                                    '<div class="d-flex justify-content-between align-items-center">'+
-                                        '<div class="btn-group">'+
-                                            '<button type="button" class="btn btn-info" data-toggle="modal" data-target="#exampleModal" onClick="controller.getAuthorDetails('+element["userId"]+');">Author Details</button>'+                                            
-                                        '</div>'+
-                                    '</div>'+
-                                '</div>'+
-                            '</div>'+
-                        '</div>';
-        });
-        model.contentDiv.insertAdjacentHTML( 'beforeend', html );
-    },
-    renderPopup : (resp) => {
-        var html = '<p class="alert alert-primary"> Name : '+resp.name+'</p>';
-        html += '<p class="alert alert-secondary"> Email : '+resp.email+'</p>';
-        html += '<p class="alert alert-success"> Company : '+resp.company.name+'</p>';
-        html += '<p class="alert alert-danger"> Address : '+resp.address.suite+'<br/>'+resp.address.street+'<br/>'+resp.address.city+'<br/>'+resp.address.zipcode+'</p>';
-        html += '<p class="alert alert-warning"> Website : '+resp.website+'</p>';
-        model.modalDiv.insertAdjacentHTML( 'beforeend', html );
-    }
-};
-controller.init();
+let authorData = [];
+
+function init(){
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(resp => resp.json())
+    .then(resp => renderPosts(resp))
+    .catch(resp => {
+        document.getElementById("exampleModalLabel").innerText = 'Error';
+        document.getElementById("modal-body").innerHTML = resp;
+        $('#exampleModal').modal('show');
+    });
+}
+
+function getAuthorDetails(id){
+    let getAuthor = checkAuthor(id);
+    if(getAuthor){
+        renderPopup(getAuthor);
+    }else{
+        fetch('https://jsonplaceholder.typicode.com/users/'+id)
+        .then(resp => resp.json())
+        .then(resp => {           
+            authorData.push(resp);
+            renderPopup(resp);
+        })      
+        .catch(resp => console.log(resp));
+    }    
+}
+
+function renderPosts(resp){
+    let html = '';
+    resp.forEach(element => {
+    html += '<div class="card mb-4">'+
+                '<div class="card-header text-info">'+element['title']+'</div>'+
+                '<div class="card-body text-info">'+
+                    '<p class="card-text">'+element['body']+'</p>'+                                                                
+                '</div><div class="card-footer"><small class="text-muted"><button type="button" class="btn btn-info custom" data-toggle="modal" data-target="#exampleModal" onClick="getAuthorDetails('+element["userId"]+');">Author Details</button></small></div>'+
+            '</div>';
+    });
+    document.getElementById("content").innerHTML = html;
+}
+
+function renderPopup(resp){
+    var html = '<p class="alert alert-primary"> Name : '+resp.name+'</p>';
+    html += '<p class="alert alert-secondary"> Email : '+resp.email+'</p>';
+    html += '<p class="alert alert-success"> Company : '+resp.company.name+'</p>';
+    html += '<p class="alert alert-danger"> Address : '+resp.address.suite+'<br/>'+resp.address.street+'<br/>'+resp.address.city+'<br/>'+resp.address.zipcode+'</p>';
+    html += '<p class="alert alert-warning"> Website : '+resp.website+'</p>';
+    document.getElementById("modal-body").innerHTML = html;
+}
+
+function checkAuthor(id){
+    let authData = authorData.find(author => {return author.id == id});
+    return authData;
+}
+
+init();
